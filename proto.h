fc468e675c247897947cd749d0d5e12884af7512
Qi Li <lq3552@gmail.com>
2022-10-08 19:21:16 +0200

    Clean up proto.h

--- proto.h
+++ proto.h
@@ -2,13 +2,8 @@
 
 #ifndef PROTO_H
 #define PROTO_H
+
 int SetParameter(Grid &grid, FILE* fptr);
-
-int Flux(double *d, double *E, double *e, double *u, double *p, double *cs, double **U, double **F,
-		int GridRank,int* GridDimension,int NumberofGhostZones);
-int Hydro_Update(double dx, double dt, double *d, double *E, double *e, double *u, double *p, double *cs, double **U, double **F,
-		int GridRank,int* GridDimension,int NumberofGhostZones);
-
 /* Riemann solver */
 int Riemann(double *WL, double cL, double *WR, double cR, double *WS);
 
