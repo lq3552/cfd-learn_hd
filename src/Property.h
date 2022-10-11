65c16d26bae5f5b4f4f838894d277c1a9e4136d2
Qi Li <lq3552@gmail.com>
2022-10-11 21:14:36 +0200

    Using Property class to encapsulate Global parameters

--- src/Property.h
+++ src/Property.h
@@ -1,1 +1,24 @@
-/* Property class, containing members that have private setter and  */
+/* Property class, containing members that have
+   private setter and public getter */
+#include "Global.h"
+
+template <typename T>
+class Property
+{
+	T m_value;
+	/* override implicit type cast */
+	public:
+		operator T() const
+		{
+			return m_value;
+		}
+	/* overload assignment */
+	private:
+		Property<T>& operator= (const T& value)
+		{
+			m_value = value;
+			return *this;
+		}
+
+		friend class Global;
+};
